dcf93656bb706beeea2c5db6933d7ca9daff7fce
Muhammad Hafiz Hanan <muhammad.hafiz43@ui.ac.id>
2025-09-10 11:59:44 +0700

    perbaikan yang tadi

--- toko_bola_hafiz/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    context = {
-        'npm': '12345678',
-        'name': 'Hafiz',
-        'class': 'PBP'
-    }
-    return render(request, 'index.html', context)
