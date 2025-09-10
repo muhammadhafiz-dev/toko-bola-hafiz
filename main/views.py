c2fbf776fd4dea8258882cb45be576b5ef775d22
Muhammad Hafiz Hanan <muhammad.hafiz43@ui.ac.id>
2025-09-10 20:01:48 +0700

    Mengganti nama npm kelas

--- main/views.py
+++ main/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
+        'npm' : '2406437994',
+        'name': 'Muhammad Hafiz Hanan',
         'class': 'PBP A'
     }
 
