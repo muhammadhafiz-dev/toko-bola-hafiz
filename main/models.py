2e10b474fa56071e244e297084e45ff0d9ddd45b
Muhammad Hafiz Hanan <muhammad.hafiz43@ui.ac.id>
2025-09-16 18:38:13 +0700

    tugas 3 mulai

--- main/models.py
+++ main/models.py
@@ -19,8 +19,16 @@ class Product(models.Model):
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     is_featured = models.BooleanField(default=False)
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    product_views = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True) # Field created_at dan tidak dimasukkan ke list fields didalam forms.py karena ditambahkan secara otomatis.
     
     def __str__(self):
         return self.name
+
+    @property
+    def is_product_hot(self):
+        return self.product_views > 20
+        
+    def increment_views(self):  # ini bakal ada di def show_product di views.py
+        self.product_views += 1  # ini dari product_views diatas dari models.py
+        self.save()
